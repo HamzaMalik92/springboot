33f2835c4c14f481656d6548b02a0b676779c1f7
Hamza Malik <hamzamalikcs20@gmail.com>
2023-10-09 13:30:27 +0500

    Update NotificationController.java

--- src/main/java/com/example/demo/NotificationController.java
+++ src/main/java/com/example/demo/NotificationController.java
@@ -22,7 +22,7 @@ public class NotificationController {
                 "    \"data\": {\n" +
                 "        \"title\": \"" + notification.getTitle() + "\",\n" +
                 "        \"description\": \"" + notification.getDescription() + "\",\n" +
-                "        \"type\":"+ notification.getType() +"\n" +
+                "        \"description\": \"" + notification.getType() + "\",\n" +
                 "     }\n" +
                 "}";
 
