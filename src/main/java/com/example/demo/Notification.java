63ba876403e0c986abd20781bbc42d2488202e33
Hamza Malik <hamzamalikcs20@gmail.com>
2024-08-27 22:27:14 +0500

    working before migration

--- src/main/java/com/example/demo/Notification.java
+++ src/main/java/com/example/demo/Notification.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 
 public class Notification {
     @NotBlank
@@ -9,64 +14,18 @@ public class Notification {
     String title;
 
     Type type;
+
+
     @NotBlank
     @Size(min = 10)
     String description;
-
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public Notification(String title, Type type, String description, String version, String topic) {
-        this.title = title;
-        this.type = type;
-        this.description = description;
-        this.version = version;
-        this.topic = topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
+    String actionType="";
+    String actionData="";
     String version = "";  // Default value set to an empty string
 
     @NotBlank
     @Size(min = 3)
     String topic;
 
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
--- src/main/java/com/example/demo/NotificationController.java
+++ src/main/java/com/example/demo/NotificationController.java
@@ -2,37 +2,78 @@ package com.example.demo;
 
 import jakarta.validation.Valid;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController()
+@RequestMapping("/sendNotification")
 public class NotificationController {
-    @PostMapping("sendNotification")
-    public ResponseEntity<?> sendNotification(@Valid @RequestBody Notification notification) {
+    @PostMapping("all")
+    public ResponseEntity<?> toAll(@Valid @RequestBody Notification notification) {
 
         if(notification.getVersion()!=null&&!notification.getVersion().isEmpty()&&notification.getVersion().length()!=3){
-            return ResponseEntity.ok().body("Invalid app version");
+            return ResponseEntity.badRequest().body("Invalid app version");
         }
 
-        // FCM server endpoint URL
-        String url = "https://fcm.googleapis.com/fcm/send";
-        // FCM request data
-        String to = "/topics/"+notification.getTopic();
+        String requestBody = getRequestBody("", notification);
 
-        // Construct the request body using Notification object
-        String requestBody = "{\n" +
+        ResponseEntity<String> stringResponseEntity = SendToFCM(requestBody);
+
+        Map<String, String> responseMap = new HashMap<>();
+        responseMap.put("requestBody", notification.toString());
+        responseMap.put("responseBody", stringResponseEntity.getStatusCode().toString());
+
+        // Return the map as the response
+        return ResponseEntity.ok().body(responseMap);
+    }
+
+
+    @PostMapping("one/{uid}")
+    public ResponseEntity<?> toOne(@PathVariable String uid, @Valid @RequestBody Notification notification) {
+
+        if (notification.getVersion() != null && !notification.getVersion().isEmpty() && notification.getVersion().length() != 3) {
+            return ResponseEntity.badRequest().body("Invalid app version");
+        }
+        if (uid == null || uid.length() != 36) {
+            return ResponseEntity.badRequest().body("Invalid uId");
+        }
+
+        String requestBody = getRequestBody(uid, notification);
+
+        ResponseEntity<String> stringResponseEntity = SendToFCM(requestBody);
+
+        Map<String, String> responseMap = new HashMap<>();
+        responseMap.put("requestBody", notification.toString());
+        responseMap.put("responseBody", stringResponseEntity.getStatusCode().toString());
+
+        // Return the map as the response
+        return ResponseEntity.ok().body(responseMap);
+    }
+
+    // Construct the request body using Notification object
+    private static String getRequestBody(String uid, Notification notification) {
+        String to = "/topics/" + notification.getTopic();
+        return "{\n" +
                 "    \"to\": \"" + to + "\",\n" +
                 "    \"data\": {\n" +
                 "        \"title\": \"" + notification.getTitle() + "\",\n" +
                 "        \"description\": \"" + notification.getDescription() + "\",\n" +
                 "        \"version\": \"" + notification.getVersion() + "\",\n" +
+                "        \"actionData\": \"" + notification.getActionData() + "\",\n" +
+                "        \"actionType\": \"" + notification.getActionType() + "\",\n" +
                 "        \"type\": \"" + notification.getType().name() + "\",\n" +
-                "     }\n" +
+                "        \"uid\": \"" + uid + "\"\n" +
+                "        \"topic\": \"" + notification.getTopic() + "\"\n" +
+                "    }\n" +
                 "}";
+    }
 
-        // Set headers
+    private ResponseEntity<String> SendToFCM(String requestBody) {
+        // FCM server endpoint URL
+        String url = "https://fcm.googleapis.com/fcm/send";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -51,6 +92,6 @@ public class NotificationController {
         System.out.println("Response: " + response.getStatusCode());
         System.out.println("Response Body: " + response.getBody());
 
-        return ResponseEntity.ok().body(notification);
+        return response;
     }
 }
