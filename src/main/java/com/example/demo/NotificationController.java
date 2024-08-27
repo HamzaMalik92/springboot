8f106c0c7d79df01c4d403f397c0bbfb552b0d6d
Hamza Malik <hamzamalikcs20@gmail.com>
2024-08-27 22:52:09 +0500

    migrated fcm api to latest

--- src/main/java/com/example/demo/NotificationController.java
+++ src/main/java/com/example/demo/NotificationController.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import jakarta.validation.Valid;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController()
 @RequestMapping("/sendNotification")
 public class NotificationController {
+
+    private static final String FCM_URL_TEMPLATE = "https://fcm.googleapis.com/v1/projects/%s/messages:send";
+    private static final String PROJECT_NAME = "expense-tracker-f1330";  // Replace with your project name
+    private static final String SERVICE_ACCOUNT_FILE_PATH = "src/main/resources/assets/json/google-services.json";
+
     @PostMapping("all")
     public ResponseEntity<?> toAll(@Valid @RequestBody Notification notification) {
 
@@ -49,15 +59,14 @@ public class NotificationController {
         responseMap.put("requestBody", notification.toString());
         responseMap.put("responseBody", stringResponseEntity.getStatusCode().toString());
 
-        // Return the map as the response
         return ResponseEntity.ok().body(responseMap);
     }
 
     // Construct the request body using Notification object
     private static String getRequestBody(String uid, Notification notification) {
-        String to = "/topics/" + notification.getTopic();
         return "{\n" +
-                "    \"to\": \"" + to + "\",\n" +
+                "  \"message\": {\n" +
+                "    \"topic\": \"" + notification.getTopic() + "\",\n" +
                 "    \"data\": {\n" +
                 "        \"title\": \"" + notification.getTitle() + "\",\n" +
                 "        \"description\": \"" + notification.getDescription() + "\",\n" +
@@ -65,19 +74,20 @@ public class NotificationController {
                 "        \"actionData\": \"" + notification.getActionData() + "\",\n" +
                 "        \"actionType\": \"" + notification.getActionType() + "\",\n" +
                 "        \"type\": \"" + notification.getType().name() + "\",\n" +
-                "        \"uid\": \"" + uid + "\"\n" +
+                "        \"uid\": \"" + uid + "\",\n" +
                 "        \"topic\": \"" + notification.getTopic() + "\"\n" +
                 "    }\n" +
+                "  }\n" +
                 "}";
     }
 
     private ResponseEntity<String> SendToFCM(String requestBody) {
         // FCM server endpoint URL
-        String url = "https://fcm.googleapis.com/fcm/send";
+        String url = String.format(FCM_URL_TEMPLATE, PROJECT_NAME);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "key=AAAAUfOZKes:APA91bE_5j8k0UvN7gVZb-bdQONtCnXD95zqP875bDt9vhpq5z6K7bbiM_aiYmyhojIC_itVIff9v-ZXn94Ib94xIjGWHlVAP6hbsjE1LMavYWQFxGobMPYSUuJiUKzKpL-TnuxiRfIQ");
+        headers.setBearerAuth(getAccessToken());
 
         // Create the request entity
         HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
@@ -93,5 +103,17 @@ public class NotificationController {
         System.out.println("Response Body: " + response.getBody());
 
         return response;
+    }
+
+    private String getAccessToken() {
+        try (FileInputStream serviceAccountStream = new FileInputStream(SERVICE_ACCOUNT_FILE_PATH)) {
+            GoogleCredentials credentials = ServiceAccountCredentials.fromStream(serviceAccountStream);
+            credentials = credentials.createScoped("https://www.googleapis.com/auth/cloud-platform");
+            credentials.refreshIfExpired();
+            AccessToken token = credentials.getAccessToken();
+            return token.getTokenValue();
+        } catch (IOException e) {
+            throw new RuntimeException("Error obtaining OAuth2 access token", e);
+        }
     }
 }
