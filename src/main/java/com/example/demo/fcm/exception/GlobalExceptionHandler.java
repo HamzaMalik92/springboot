40354abaaefa95a6524ee008654f58ad8d2c02a2
Hamza Malik <hamzamalikcs20@gmail.com>
2024-10-11 11:19:37 +0500

    HashMap - LinkedHashMap

--- src/main/java/com/example/demo/fcm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/fcm/exception/GlobalExceptionHandler.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.HashMap;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashMap;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({IllegalArgumentException.class,MethodArgumentNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<HashMap<String, String>> handleValidationExceptions(Exception ex) {
-        HashMap<String, String> errors = new HashMap<>();
+    public ResponseEntity<LinkedHashMap<String, String>> handleValidationExceptions(Exception ex) {
+        LinkedHashMap<String, String> errors = new LinkedHashMap<>();
         if(ex instanceof MethodArgumentNotValidException methodArgumentNotValidException){
             methodArgumentNotValidException.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
@@ -39,8 +39,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<HashMap<String, String>> handleAllExceptions(Exception ex) {
-        HashMap<String, String> errorResponse = new HashMap<>();
+    public ResponseEntity<LinkedHashMap<String, String>> handleAllExceptions(Exception ex) {
+        LinkedHashMap<String, String> errorResponse = new LinkedHashMap<>();
         errorResponse.put("error", "Internal Server Error");
         errorResponse.put("message", ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
